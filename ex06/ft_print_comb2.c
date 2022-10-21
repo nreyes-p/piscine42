e90307a034208cbc58cec97cf5174d34a4074ebf
Norman Angel Reyes Pimentel <nreyes-p@c1r5s5.42malaga.com>
2022-10-21 13:49:49 +0200

    corregido ex06

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*   ft_print_comb2_dev.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nreyes-p <nreyes-p@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 17:47:48 by nreyes-p          #+#    #+#             */
-/*   Updated: 2022/10/20 18:20:20 by nreyes-p         ###   ########.fr       */
+/*   Updated: 2022/10/21 13:42:48 by nreyes-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,13 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_write_digits(char digit1, char digit2, char digit3)
+void	ft_write_digits(int number1, int number2, bool lastdigit)
 {
-	bool	lastdigit;
-
-	lastdigit = false;
-	ft_putchar(digit1);
-	ft_putchar(digit2);
-	ft_putchar(digit3);
-	if (digit1 == '7' && digit3 == '9')
-	{
-		lastdigit = true;
-	}
+	ft_putchar(48 + number1 / 10);
+	ft_putchar(48 + number1 % 10);
+	ft_putchar(' ');
+	ft_putchar(48 + number2 / 10);
+	ft_putchar(48 + number2 % 10);
 	if (lastdigit != true)
 	{
 		ft_putchar(',');
@@ -39,34 +34,22 @@ void	ft_write_digits(char digit1, char digit2, char digit3)
 
 void	ft_print_comb2(void)
 {
-	int	number1;
-	int	number2;
+	int		number1;
+	int		number2;
+	bool	lastdigit;
 
 	number1 = 0;
-	ft_putchar(number1);
 	while (number1 <= 99)
 	{
-		number1 = number1 + 1;
-		ft_putchar(number1);
-	}
-	/*while (digit1 <= '7')
-	{
-		digit2 = digit1 + 1;
-		while (digit2 <= '8')
+		number2 = number1 + 1;
+		while (number2 <= 99)
 		{
-			digit3 = digit2 + 1;
-			while (digit3 <= '9')
-			{
-				if (digit2 != digit3 && digit1 != digit3 && digit1 != digit2)
-				{
-					ft_write_digits(digit1, digit2, digit3);
-				}
-				digit3++;
-			}
-			digit2++;
+			lastdigit = (number1 == 98 && number2 == 99);
+			ft_write_digits(number1, number2, lastdigit);
+			number2++;
 		}
-		digit1++;
-	}*/
+		number1++;
+	}
 }
 
 int	main(void)
