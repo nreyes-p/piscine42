1ad819578f6497c67c3dcf29e11e13e2ed847299
Norman Angel Reyes Pimentel <nreyes-p@c1r7s3.42malaga.com>
2022-10-26 13:24:04 +0200

    corrigiendo ex06

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -1,31 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2_dev.c                               :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nreyes-p <nreyes-p@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 17:47:48 by nreyes-p          #+#    #+#             */
-/*   Updated: 2022/10/21 13:42:48 by nreyes-p         ###   ########.fr       */
+/*   Updated: 2022/10/26 13:20:32 by nreyes-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdbool.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_write_digits(int number1, int number2, bool lastdigit)
+void	ft_write_digits(int number1, int number2)
 {
 	ft_putchar(48 + number1 / 10);
 	ft_putchar(48 + number1 % 10);
 	ft_putchar(' ');
 	ft_putchar(48 + number2 / 10);
 	ft_putchar(48 + number2 % 10);
-	if (lastdigit != true)
+	if (number1 == 98 && number2 == 99)
+	{
+		return ;
+	}
+	else
 	{
 		ft_putchar(',');
 		ft_putchar(' ');
@@ -36,24 +39,16 @@ void	ft_print_comb2(void)
 {
 	int		number1;
 	int		number2;
-	bool	lastdigit;
 
 	number1 = 0;
-	while (number1 <= 99)
+	while (number1 <= 98)
 	{
 		number2 = number1 + 1;
 		while (number2 <= 99)
 		{
-			lastdigit = (number1 == 98 && number2 == 99);
-			ft_write_digits(number1, number2, lastdigit);
+			ft_write_digits(number1, number2);
 			number2++;
 		}
 		number1++;
 	}
-}
-
-int	main(void)
-{
-	ft_print_comb2();
-	return (0);
 }
